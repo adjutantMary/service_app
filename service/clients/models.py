40c4362360e7a28a47f9681a0b265993036f64e7
Mariya Polshkova <polshkovamamariya@gmail.com>
2025-03-25 13:28:15 +0300

    models refactor

--- service/clients/models.py
+++ service/clients/models.py
@@ -5,4 +5,7 @@ class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     company_name = models.CharField(max_length=100)
     full_adress = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"Client: {self.company_name}"
 
--- service/services/models.py
+++ service/services/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from clients.models import Client
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    full_price = models.PositiveIntegerField()
+    
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Базовый'),
+        ('student', 'Студент'),
+        ('business', 'Бизнес'),
+    )
+    
+    plan_type = models.CharField(max_length=10, choices=PLAN_TYPES)
+    discount_percent = models.PositiveIntegerField(default=0, 
+                                                   validators=[
+                                                       MaxValueValidator(100)
+                                                       ])
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.PROTECT, related_name='subscriptions')
+    service = models.ForeignKey(Service, on_delete=models.PROTECT, related_name='subscriptions')
+    plan = models.ForeignKey(Plan, on_delete=models.PROTECT, related_name="subscriptions")
+    
